f80e49222f1a18e1f1dd17dac4d62f94e22b4181
Gabriel Florit <gabriel.florit@velir.com>
2012-05-31 19:08:57 -0400

    added ball/slider poc

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ var aigua = (function () {
 $(function() {
 
 	d3.text(aigua.samples[0], function(data) {
-
 		aigua.codeMirror = CodeMirror(document.getElementById('code'), {
 			lineNumbers: true,
 			matchBrackets: true,
@@ -19,7 +18,20 @@ $(function() {
 			theme: 'lesser-dark',
 			value: data
 		});
+	});
 
+	var ball = $('#ball');
+	var slider = $('#slider');
+	ball.draggable({
+		axis: 'x',
+		drag: function(ui, event) {
+			if (ball.position().top > slider.height() - ball.height()) {
+				slider.height(slider.height() + 10);
+			}
+			if (ball.position().left > slider.width() - ball.width()) {
+				slider.width(slider.width() + 10);
+			}
+		}
 	});
 
 });
