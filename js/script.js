685e2ebff138ba0c0f6487476937658b92b3fd0e
Gabriel Florit <gabriel.florit@velir.com>
2012-06-01 16:12:43 -0400

    figured out how to handle alt-key on codemirror

--- js/script.js
+++ js/script.js
@@ -10,23 +10,43 @@ var aigua = (function () {
 
 $(function() {
 
-	// d3.text(aigua.samples[0], function(data) {
-	// 	aigua.codeMirror = CodeMirror(document.getElementById('code'), {
-	// 		lineNumbers: true,
-	// 		matchBrackets: true,
-	// 		mode:  'javascript',
-	// 		theme: 'lesser-dark',
-	// 		value: data
+	d3.text(aigua.samples[0], function(data) {
+		aigua.codeMirror = CodeMirror(document.getElementById('code'), {
+			extraKeys: {
+				'Alt-Alt': function(cm) {
+					alert('yo');
+				}
+			},
+			lineNumbers: true,
+			matchBrackets: true,
+			mode:  'javascript',
+			theme: 'lesser-dark',
+			value: data
+		});
+	});
+
+	// $('#number').on('mousedown', function(e) {
+	// 	$('#number').draggable({
+	// 		axis: 'x',
+	// 		drag: function(ui, event) {
+	// 		}
 	// 	});
+
+
+
+	// 	// var handle = $('#handle');
+	// 	// handle.css('left', $(this).position().left);
+	// 	// handle.css('top', $(this).position().top + $(this).height()/2 - handle.height()/2);
+	// 	// handle.css('width', $(this).width());
 	// });
 
-	var ball = $('#ball');
-	var bar = $('#bar');
-	var borderWidth = 1;
-	var increaseStep = 5;
-	ball.draggable({
-		axis: 'x',
-		drag: function(ui, event) {
+	// var handle = $('#handle');
+	// var bar = $('#bar');
+	// var borderWidth = 1;
+	// var increaseStep = 5;
+	// handle.draggable({
+	// 	axis: 'x',
+	// 	drag: function(ui, event) {
 
 			// prevent dragging past left edge of bar
 			// if (bar.position().left - ball.position().left > -borderWidth) {
@@ -41,32 +61,32 @@ $(function() {
 			// }
 
 			// if we're at left edge of bar, increase bar width
-			if (bar.position().left - ball.position().left >= -borderWidth) {
-				bar.animate({
-					left: '-=' + increaseStep,
-					width: '+=' + increaseStep
-				}, 0);
-			}
+			// if (bar.position().left - ball.position().left >= -borderWidth) {
+			// 	bar.animate({
+			// 		left: '-=' + increaseStep,
+			// 		width: '+=' + increaseStep
+			// 	}, 0);
+			// }
 
-			// if we're at right edge of bar, increase bar width
-			if (ball.position().left + ball.width() >= bar.position().left + bar.width()) {
-				bar.animate({
-					width: '+=' + increaseStep
-				}, 0);
-			}
-		},
-		stop: function(ui, event) {
-			// if we're past left edge of bar, bring back inside
-			if (bar.position().left - ball.position().left > -borderWidth) {
-				ball.css('left', bar.position().left + borderWidth);
-			}
+			// // if we're at right edge of bar, increase bar width
+			// if (ball.position().left + ball.width() >= bar.position().left + bar.width()) {
+			// 	bar.animate({
+			// 		width: '+=' + increaseStep
+			// 	}, 0);
+			// }
+	// 	},
+	// 	stop: function(ui, event) {
+	// 		// if we're past left edge of bar, bring back inside
+	// 		// if (bar.position().left - ball.position().left > -borderWidth) {
+	// 		// 	ball.css('left', bar.position().left + borderWidth);
+	// 		// }
 
-			// if we're past right edge of bar, bring back inside
-			if (ball.position().left + ball.width() > bar.position().left + bar.width()) {
-				ball.css('left', bar.position().left + bar.width() - ball.width() + borderWidth);
-			}
-		}
-	});
+	// 		// // if we're past right edge of bar, bring back inside
+	// 		// if (ball.position().left + ball.width() > bar.position().left + bar.width()) {
+	// 		// 	ball.css('left', bar.position().left + bar.width() - ball.width() + borderWidth);
+	// 		// }
+	// 	}
+	// });
 
 });
 
