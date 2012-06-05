0890915658b97e70a62d20917007d84b36b5970b
Gabriel Florit <gabriel.florit@velir.com>
2012-06-04 21:47:00 -0400

    disabling fill - focusing on auto bar width

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@
 
 var aigua = (function () {
 	return {
-		lineHeight: 19,
 		borderWidth: Number($('#bar').css('border-width').replace('px', '')),
-		samples: ['data/chord.txt']
+		lineHeight: 19,
+		samples: ['data/chord.txt'],
+		startingBarWidth: 200
 	}
 }());
 
@@ -65,8 +66,16 @@ $(function() {
 					// reset filler width
 					$('#filler').width(0);
 
+					// reset bar width
+					$('#bar').width(aigua.startingBarWidth);
+
 					// hide the bar
 					$('#bar').hide();
+
+					// grab the current token and deselect it
+					var cursor = cm.getCursor();
+					var token = cm.getTokenAt(cursor);
+					cm.setSelection({line: cursor.line, ch: token.end}, {line: cursor.line, ch: token.end});
 				}
 			},
 			lineNumbers: true,
@@ -85,20 +94,54 @@ $(function() {
 			var markerCenter = $('#marker').offset().left;
 			var handleOffset = handleCenter - markerCenter;
 
-			// are we dragging to the right?
+			// is the dragging cursor to the right of the marker?
 			if (handleOffset > 0) {
-				$('#filler').css('left', $('#bar').width()/2);
-				$('#filler').width(handleOffset);
 
-			// are we dragging to the left?
+				// are we dragging past the initial bar width?
+				if (handleOffset > aigua.startingBarWidth/2 - (aigua.borderWidth)) {
+
+					// set bar right edge to dragging position
+					$('#bar').width(handleCenter - $('#bar').offset().left);
+				}
+
+				// reset the width, since fast drags won't trigger a drag call every pixel. 
+				else {
+					$('#bar').width(aigua.startingBarWidth);
+				}
+
+				// $('#filler').css('left', $('#bar').width()/2);
+				// $('#filler').width(handleOffset);
+				// did we reach the right edge of the bar?
+				// if (handleCenter - ($('#bar').offset().left + $('#bar').width()) > 0) {
+				// 	$('#bar').width(handleCenter - $('#bar').offset().left);
+				// }
+
+			// is the dragging cursor to the left of the marker?
 			} else if (handleOffset < 0) {
-				$('#filler').css('left', handleOffset + $('#bar').width()/2);
-				$('#filler').width(-handleOffset);
 
-			// are we right at the middle?
+				// are we dragging past the initial bar width?
+				if (-handleOffset> aigua.startingBarWidth/2) {
+
+					// set bar left edge to dragging position
+					$('#bar').width(-handleOffset + aigua.startingBarWidth/2);
+					$('#bar').css('left', handleCenter - aigua.borderWidth);
+				}
+
+				// reset the width, since fast drags won't trigger a drag call every pixel. 
+				else {
+					$('#bar').width(aigua.startingBarWidth);
+					$('#bar').css('left', markerCenter - aigua.startingBarWidth/2 - aigua.borderWidth);
+				}
+
+
+
+				// $('#filler').css('left', handleOffset + $('#bar').width()/2);
+				// $('#filler').width(-handleOffset);
+
+			// are we at the middle?
 			} else {
-				$('#filler').css('left', $('#bar').width()/2);
-				$('#filler').width(0);
+				// $('#filler').css('left', $('#bar').width()/2);
+				// $('#filler').width(0);
 			}
 		},
 		stop: function(ui, event) {
