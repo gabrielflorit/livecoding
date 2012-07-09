ffab7c283237e6143a4cb93c48698f402dfaf27c
Gabriel Florit <gabriel.florit@velir.com>
2012-07-09 01:49:20 -0400

    doh. removing client id/secret from repo.

--- livecoding/__init__.py
+++ livecoding/__init__.py
@@ -8,7 +8,7 @@ app.wsgi_app = GzipMiddleware(app.wsgi_app, compresslevel=5)
 
 import livecoding.views
 
-os.environ['CLIENT_ID'] = '9d827b263687bb7ad08b'
-os.environ['CLIENT_SECRET'] = '0f8854df5a14ff8c2a47ce13c0f0dfb02637d213'
-os.environ['SECRET_KEY'] = "\x7f\xfa'\x06\x15\x04\x14\x8c\x07\xd2&\xf36\xa1^\x13\x1fq\xf3\x99\xbd\xa4\x87\xa8"
+os.environ['CLIENT_ID'] = ''
+os.environ['CLIENT_SECRET'] = ''
+os.environ['SECRET_KEY'] = ""
 app.secret_key = os.getenv('SECRET_KEY')
