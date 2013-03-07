12334c25ae7fa73de0618d089832bc897d0435b8
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-07 18:35:17 -0500

    housekeeping

--- livecoding/__init__.py
+++ livecoding/__init__.py
@@ -8,7 +8,10 @@ app.wsgi_app = GzipMiddleware(app.wsgi_app, compresslevel=5)
 
 # os.environ['CLIENT_ID'] = ''
 # os.environ['CLIENT_SECRET'] = ''
-#os.environ['SECRET_KEY'] = ""
+# os.environ['MONGOHQ_URL'] = ""
+
+# os.environ['SECRET_KEY'] = ""
+
 app.secret_key = os.getenv('SECRET_KEY')
 
 import livecoding.views
