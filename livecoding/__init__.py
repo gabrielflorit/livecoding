6e89f96d8202b21629d46341e49b0238ec378912
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-07 16:30:11 -0500

    housekeeping

--- livecoding/__init__.py
+++ livecoding/__init__.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 from util.gzipmiddleware import GzipMiddleware
 app.wsgi_app = GzipMiddleware(app.wsgi_app, compresslevel=5)
 
-import livecoding.views
-
-#os.environ['CLIENT_ID'] = ''
-#os.environ['CLIENT_SECRET'] = ''
+# os.environ['CLIENT_ID'] = ''
+# os.environ['CLIENT_SECRET'] = ''
 #os.environ['SECRET_KEY'] = ""
 app.secret_key = os.getenv('SECRET_KEY')
+
+import livecoding.views
+
+
