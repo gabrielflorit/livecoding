bdaf8219ccc98f7273586d8bb6da7e4a4a965406
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-08 22:25:32 -0500

    trying easier cache bustin

--- livecoding/views.py
+++ livecoding/views.py
@@ -4,10 +4,26 @@ import requests
 import json
 import pymongo
 from livecoding import app
-from flask import render_template, send_from_directory, redirect, session, request
+from flask import render_template, send_from_directory, redirect, session, request, url_for
 from requests import post
 from pymongo import MongoClient
 from bson import json_util
+
+
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 
