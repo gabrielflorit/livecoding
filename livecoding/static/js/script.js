a002a71ea42eebe440a30c8cab6ad994e92b95fe
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-21 23:47:02 -0400

    moved snippets to separate file

--- livecoding/static/js/script.js
+++ livecoding/static/js/script.js
@@ -991,68 +991,7 @@ var aigua = (function () {
 		pulseNumbersInterval: null,
 		screenLayouts: ['fullscreen mode (horizontal)', 'fullscreen mode (vertical)', 'sketchpad mode'],
 		slider: null,
-		snippets: [
-			{
-				keyword: 'bor',
-				snippet: 'border: solid #FF0000 1px;',
-				mode: 'css'
-			},
-			{
-				keyword: 'bac',
-				snippet: 'background: #FF0000;',
-				mode: 'css'
-			},
-			{
-				keyword: 'mar',
-				snippet: 'margin: 0em 0em 0em 0em;',
-				mode: 'css'
-			},
-			{
-				keyword: 'pad',
-				snippet: 'padding: 0em 0em 0em 0em;',
-				mode: 'css'
-			},
-			{
-				keyword: 'each',
-				snippet: '.each(function(v, i) {})',
-				mode: 'javascript'
-			},
-			{
-				keyword: 'map',
-				snippet: '.map(function(v, i) {})',
-				mode: 'javascript'
-			},
-			{
-				keyword: 'find',
-				snippet: '.find(function(v, i) {})',
-				mode: 'javascript'
-			},
-			{
-				keyword: 'filter',
-				snippet: '.filter(function(v, i) {})',
-				mode: 'javascript'
-			},
-			{
-				keyword: 'reject',
-				snippet: '.reject(function(v, i) {})',
-				mode: 'javascript'
-			},
-			{
-				keyword: 'sort',
-				snippet: '.sortBy(function(v, i) {})',
-				mode: 'javascript'
-			},
-			{
-				keyword: 'group',
-				snippet: '.groupBy(function(v, i) {})',
-				mode: 'javascript'
-			},
-			{
-				keyword: 'count',
-				snippet: '.countBy(function(v, i) {})',
-				mode: 'javascript'
-			}
-		],
+		snippets: lc.snippets,
 		startingBarWidth: 300,
 		triangle: null,
 		triangleHeight: 5,
