3487e4239d81ea9d49aa10f26c8299ff7850652a
Gabriel Florit <gabrielflorit@gmail.com>
2013-03-31 03:35:52 -0400

    part of #71: get slider key to work in linux

--- livecoding/static/js/editor/sliderKey.js
+++ livecoding/static/js/editor/sliderKey.js
@@ -2,9 +2,9 @@ var lc = lc || {};
 lc.getSliderKey = function(os) {
 
 	var key = {};
-	key.Name        = os == 'Mac' ? 'Alt-Alt' : (os == 'Linux' ?  'Ctrl' : 'Ctrl-Ctrl' ); 
-	key.DisplayName = os == 'Mac' ?     'Alt' : (os == 'Linux' ?  'Ctrl' :      'Ctrl' ); 
-	key.Code        = os == 'Mac' ?        18 : (os == 'Linux' ?      17 :          17 );
+	key.Name        = os == 'Mac' ? 'Alt-Alt' : (os == 'Linux' ?  'Ctrl-Ctrl' : 'Ctrl-Ctrl' ); 
+	key.DisplayName = os == 'Mac' ?     'Alt' : (os == 'Linux' ?  'Ctrl'      :      'Ctrl' ); 
+	key.Code        = os == 'Mac' ?        18 : (os == 'Linux' ?      17      :          17 );
 
 	return key;
 
