d4cdba4dc266dbd11c41ebc01d468e0c0add68c4
Gabriel Florit <gabrielflorit@gmail.com>
2014-10-25 01:52:20 -0400

    add poc of gistsaving

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,1 +1,8 @@
-console.log('it worked');
+var CodeMirror = require('codemirror');
+require('../../node_modules/codemirror/lib/codemirror.css');
+
+var myCodeMirror = CodeMirror(document.body);
+
+$('button').click(function() {
+	alert('clicked');
+});
