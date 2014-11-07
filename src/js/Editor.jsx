cb639139548349b92bd108e73da8de33913724cc
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-07 14:21:45 -0500

    init cm

--- src/js/Editor.jsx
+++ src/js/Editor.jsx
@@ -1,14 +1,26 @@
 require('../css/editor.css');
 var React = require('react');
+var CodeMirror = require('codemirror');
 
 var Editor = React.createClass({
+
+	codemirror: null,
 
 	render: function() {
 		return (
 			<div className='editor'>
-				the editor
+				<div className='editor-codemirror'>
+				</div>
 			</div>
 		);
+	},
+
+	componentDidMount: function() {
+		var self = this;
+
+		this.codemirror = CodeMirror(this.getDOMNode().querySelector('.editor-codemirror'), {
+			lineNumbers: true
+		});
 	}
 
 });
