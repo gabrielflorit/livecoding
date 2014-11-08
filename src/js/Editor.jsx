940cdee83a9d2f303183c48205eb6410cdb4f0f2
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-07 23:19:14 -0500

    better docs

--- src/js/Editor.jsx
+++ src/js/Editor.jsx
@@ -1,13 +1,21 @@
-require('../css/editor.css');
-var React = require('react');
-var CodeMirror = require('codemirror');
-var util = require('./util.js');
-var _ = require('lodash');
+// This component contains the code editor.
 
+// Include this component's stylesheet.
+require('../css/editor.css');
+
+// Include React.
+var React = require('react');
+
+// We'll use CodeMirror as the code editor.
+var CodeMirror = require('codemirror');
+
+// Create the component.
 var Editor = React.createClass({
 
+	// Convenience property.
 	codemirror: null,
 
+	// Render the component.
 	render: function() {
 		return (
 			<div className='editor'>
@@ -17,33 +25,43 @@ var Editor = React.createClass({
 		);
 	},
 
+	// This method is fired once, after the initial render. We'll use
+	// it to initialize the CodeMirror instance.
 	componentDidMount: function() {
 
 		var self = this;
 
+		// Initialize the CodeMirror instance with various options.
 		this.codemirror = CodeMirror(this.getDOMNode().querySelector('.editor-codemirror'), {
 			lineNumbers: true
 		});
 
+		// When the code editor's contents change,
 		this.codemirror.on('change', function(cm, o) {
 
-			// grab codemirror contents
+			// get the contents,
 			var content = cm.getValue();
 
-			// notify Livecoding of change event
+			// and pass them off to **Livecoding**.
 			self.props.onContentChange(content);
 		});
 	},
 
+	// This method is called every time **Livecoding's** state changes, e.g.
+	// when we load a new gist, or when the user makes a change to the
+	// current code editor contents.
 	shouldComponentUpdate: function(props, state) {
 
-		// if incoming content is different than current content
-		// set content on CM.
+		// Get CodeMirror's current contents.
 		var currentContent = this.codemirror.getValue();
+
+		// Don't update CodeMirror if incoming and current contents
+		// are identical. We need this because of our one-way data flow.
 		if (currentContent !== props.content) {
 			this.codemirror.setValue(props.content);
 		}
 
+		// Tell React not to manage this component's DOM.
 		return false;
 	}
 
--- src/js/Output.jsx
+++ src/js/Output.jsx
@@ -1,9 +1,16 @@
-require('../css/output.css');
-var React = require('react');
-var util = require('./util.js');
+// This component renders the current code.
 
+// Include this component's stylesheet.
+require('../css/output.css');
+
+// Include React.
+var React = require('react');
+
+// Create the component.
 var Output = React.createClass({
 
+	// Render the component. We use an iframe to prevent
+	// the user's JS/CSS from interfering with the application.
 	render: function() {
 		return (
 			<div className='output'>
@@ -12,15 +19,19 @@ var Output = React.createClass({
 		);
 	},
 
+	// Tell React not to manage this component's DOM.
 	shouldComponentUpdate: function(props, state) {
 
+		// Get the iframe.
 		var doc = this.getDOMNode().querySelector('iframe').contentWindow.document;
 
-		if (props.change === 'html') {
+		// If the current mode is `html`, replace the iframe's `body` contents.
+		if (props.mode === 'html') {
 			doc.body.innerHTML = props.html;
 		}
 
-		if (props.change === 'css') {
+		// If the current mode is `css`, replace the iframe's `style` contents.
+		if (props.mode === 'css') {
 			doc.head.querySelector('style').textContent = props.css;
 		}
 
