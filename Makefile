3d5383a4f52a9f8e8a0e3a8cff7275774225c58a
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-07 23:02:44 -0500

    better comments.

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ doc:
 
 	mkdir .doc;
 	cp src/js/*.jsx .doc;
-	grep -l '\t' .doc/*.jsx | xargs sed -i "" 's/	/    /g';
+	grep -l '\t' .doc/*.jsx | xargs sed -i "" 's/	/   /g';
 	docco -o docs -e .js .doc/*.jsx;
 	rm -rf .doc;
--- src/js/Livecoding.jsx
+++ src/js/Livecoding.jsx
@@ -1,5 +1,5 @@
-// Livecoding is the parent component. It includes all other
-// components, handling and responding to events when necessary.
+// **Livecoding** is the parent component. It includes all other
+// components. It listens and responds to events when necessary.
 // It maintains the application's state.
 
 // Include this component's stylesheet.
@@ -16,36 +16,42 @@ var Toolbar = require('./Toolbar.jsx');
 // Create the React component.
 var Livecoding = React.createClass({
 
-	// Set the initial state.
+	// Set the initial state. As the application grows, so
+	// will the number of state properties.
 	getInitialState: function() {
 		return {
 			html: '',
 			js: '',
 			css: '',
-			selected: 'html'
+			// Specify what mode we're currently editing.
+			mode: 'html'
 		};
 	},
 
+	// Each React component has a `render` method. It gets called
+	// every time the application's state changes.
 	render: function() {
+		// Get the current mode.
+		var mode = this.state.mode;
 
-		var state = this.state;
+		// Get the current mode's content.
+		var content = this.state[mode];
 
-		var selected = state.selected;
-		var content = state[selected];
-
+		// Render the application. This will recursively call
+		// `render` on all the components.
 		return (
 			<div className='livecoding'>
 				<Toolbar />
 				<div className='content'>
 					<Output
-						html={state.html}
-						js={state.js}
-						css={state.css}
-						change={selected}
+						html={this.state.html}
+						js={this.state.js}
+						css={this.state.css}
+						change={mode}
 					/>
 					<Editor
 						content={content}
-						language={selected}
+						language={mode}
 						onContentChange={this.handleContentChange}
 					/>
 				</div>
@@ -53,15 +59,24 @@ var Livecoding = React.createClass({
 		);
 	},
 
+	// Every time **Editor**'s content changes it hands **Livecoding**
+	// the new content. **Livecoding** then updates its current state
+	// with the new content. This setup enforces React's one-way data
+	// flow.
 	handleContentChange: function(content) {
-		var selected = this.state.selected;
+
+		// Get the current mode.
+		var mode = this.state.mode;
+
+		// Update application state with new content.
 		var change = {};
-		change[selected] = content;
+		change[mode] = content;
 		this.setState(change);
 	}
 
 });
 
+// Render the entire application to `#main`.
 React.render(
 	<Livecoding />,
 	document.getElementById('main')
