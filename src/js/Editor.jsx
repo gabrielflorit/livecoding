366a0c9401e6fde4fe8aad5f84b0c3942edcdbcb
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-11 01:40:06 -0500

    keep mode documents around in Editor
    this way we get code mirror undo/redo for free.

--- src/js/Editor.jsx
+++ src/js/Editor.jsx
@@ -5,6 +5,7 @@ var React      = require('react');
 
 // Include libraries.
 var PubSub     = require('pubsub-js');
+var util       = require('./util.js');
 
 // We'll use CodeMirror as the code editor.
 var CodeMirror = require('codemirror');
@@ -22,6 +23,8 @@ var Editor = React.createClass({
 			};
 		}
 	},
+
+	documents: {},
 
 	// Convenience property.
 	codemirror: null,
@@ -65,13 +68,33 @@ var Editor = React.createClass({
 	// current code editor contents.
 	shouldComponentUpdate: function(props, state) {
 
-		// Get CodeMirror's current contents.
-		var currentContent = this.codemirror.getValue();
+		// Get current CodeMirror document.
+		var currentDoc = this.codemirror.getDoc();
 
-		// Don't update CodeMirror if incoming and current contents
-		// are identical. We need this because of our one-way data flow.
-		if (currentContent !== props.content) {
-			this.codemirror.setValue(props.content);
+		// Let's compare new and current properties. First, mode.
+		// Even though Livecoding thinks there are three modes (html, js, css)
+		// we'll use the 'htmlmixed' CodeMirror mode, which lets us
+		// write html/js/css in the same document.
+		var newMode = props.mode.replace('html', 'htmlmixed');
+
+		// Get mode of current document.
+		var currentMode = currentDoc.getMode().name;
+
+		// If new mode doesn't match current mode, get the correct document
+		// (or create it first) and then swap documents.
+		var newDoc;
+		if (newMode !== currentMode) {
+			newDoc = this.documents[newMode] || CodeMirror.Doc('', newMode);
+			this.documents[currentMode] = this.codemirror.swapDoc(newDoc);
+		}
+
+		// Next up, compare content.
+		var currentContent = this.codemirror.getValue();
+		var newContent = props.content;
+
+		// If new content doesn't match current content, replace.
+		if (currentContent !== newContent) {
+			this.codemirror.setValue(newContent);
 		}
 
 		// Tell React not to manage this component's DOM.
