7e32f2d2a71690d8a15ad9a3f1a0df8625671063
Gabriel Florit <gabrielflorit@gmail.com>
2014-11-22 20:20:56 -0500

    Add js linting messages to line gutters. #116

--- src/js/javascript-lint.js
+++ src/js/javascript-lint.js
@@ -1,8 +1,38 @@
 var CodeMirror = require('codemirror');
+var util       = require('./util.js');
+var esprima    = require('esprima');
 
 CodeMirror.registerHelper('lint', 'javascript', function(text) {
 
-	var messages = [];
+	var AST;
+	var errors = [];
+
+	try {
+
+		AST = esprima.parse(text, {tolerant: true, loc: true});
+		if (AST.errors.length) {
+			errors = AST.errors;
+		}
+	} catch(e) {
+		errors = [e];
+	}
+
+	var messages = errors.map(function(error) {
+
+		return {
+			from: {
+				line: error.lineNumber - 1,
+				ch: error.column
+			},
+			to: {
+				line: error.lineNumber - 1,
+				ch: error.column + 1
+			},
+			message: error.description,
+			severity: 'error'
+		};
+
+	});
 
 	return messages;
 });
