bef95641874949c3648ee9587d7266934433ce63
Gabriel Florit <gabrielflorit@gmail.com>
2014-12-05 15:56:39 -0500

    refactor

--- src/js/util/util.js
+++ src/js/util/util.js
@@ -6,20 +6,20 @@ module.exports = {
 
 	// from https://mathiasbynens.be/notes/xhr-responsetype-json
 	getJSON: function(url) {
-	  return new Promise(function(resolve, reject) {
-		var xhr = new XMLHttpRequest();
-		xhr.open('get', url, true);
-		xhr.responseType = 'json';
-		xhr.onload = function() {
-			var status = xhr.status;
-			if (status === 200) {
-				resolve(xhr.response);
-			} else {
-				reject(status);
-			}
-		};
-		xhr.send();
-	  });
+		return new Promise(function(resolve, reject) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('get', url, true);
+			xhr.responseType = 'json';
+			xhr.onload = function() {
+				var status = xhr.status;
+				if (status === 200) {
+					resolve(xhr.response);
+				} else {
+					reject(status);
+				}
+			};
+			xhr.send();
+		});
 	}
 
 };
